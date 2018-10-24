caf774c6f47b9df72535a6aa24fa25178ab02414
120offset <42881956+120offset@users.noreply.github.com>
2018-10-23 22:52:20 -0400

    Simple RPG
    Used to teach in the fundementals of RPG

---
+++ Engine/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Item
+    {
+        //properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set;  }
+
+        //public base class constructor
+        public Item(int id, string name, string namePlural)
+        {
+            ID = id;
+            Name = name;
+            NamePlural = namePlural;
+        }
+    }
+}
---
+++ Engine/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Monster : LivingCreature
+    {
+        //properties
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
+
+        //public constructor
+        public Monster(int id, string name, int maxiumDamage, int rewardExperiencePoints,
+            int rewardGold, int currentHitPoints, 
+            int maximumHitPoints) : base(currentHitPoints, maximumHitPoints)
+        {
+            ID = id;
+            Name = name;
+            MaximumDamage = MaximumDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
+        }
+
+
+
+    }
+}
